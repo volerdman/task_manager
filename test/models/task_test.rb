08d05bed9bc053d6f043fbcfe3bb75774b80d8ec
volerdman <volerdman+github@gmail.com>
2021-04-22 00:36:27 +0400

    connections and validation

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create" do
+    author = create :manager
+    assigne = create :developer
+    task = create(:task, author: author, assignee: assigne)
+    assert task.persisted?
+  end
 end
